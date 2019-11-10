c93eb11b46cd5b4363a0a9d14b95f0ae567870cc
Shiqi Liu <liushiqi17@mails.ucas.ac.cn>
2019-11-10 21:20:24 +0800

    changed option parser.

--- src/argument.cpp
+++ src/argument.cpp
@@ -1,24 +1,21 @@
-#include <boost/program_options.hpp>
+#include <cxxopts.hpp>
 #include <iostream>
 
 std::string parse_arguments(int argc, char *argv[]) {
   std::string assets_path;
-  boost::program_options::options_description options("A program for computer graphics class");
+  cxxopts::Options options("computer-graphics", "A program for computer graphics class");
   try {
-    options.add_options()("help,h", "Display this help message.")("assets,a", boost::program_options::value<std::string>()->required(),
-                                                                  "the path to assets directory.");
-    boost::program_options::variables_map map;
-    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), map);
-    boost::program_options::notify(map);
-    if (map.count("help")) {
-      std::cout << options << std::endl;
+    options.add_options()("h,help", "Display this help message.")("a,assets", "the path to assets directory.", cxxopts::value<std::string>());
+    auto result = options.parse(argc, argv);
+    if (result.count("help")) {
+      std::cout << options.help() << std::endl;
       exit(0);
     }
-    if (map.count("assets")) {
-      assets_path = map["assets"].as<std::string>();
+    if (result.count("assets")) {
+      assets_path = result["assets"].as<std::string>();
     }
-  } catch (const boost::program_options::error &error) {
-    std::cerr << "arguments parse failed: " << error.what() << std::endl << options;
+  } catch (const cxxopts::OptionSpecException &error) {
+    std::cerr << "arguments parse failed: " << error.what() << std::endl << options.help();
     exit(1);
   }
   return assets_path;
